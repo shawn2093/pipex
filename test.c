5186f3c45415182074b088b1dacbe1652cfb87a1
shawn2093 <shawnong1993@gmail.com>
2023-12-25 05:45:25 +0800

    testing

--- test.c
+++ test.c
@@ -4,7 +4,110 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <errno.h>
- 
+static int	is_sep(char c, char set)
+{
+	if (c == set)
+		return (1);
+	return (0);
+}
+
+static int	word_count(char const *str, char set)
+{
+	int	i;
+	int	count;
+
+	i = 0;
+	count = 0;
+	while (str[i])
+	{
+		while (str[i] && is_sep(str[i], set))
+			i++;
+		if (str[i] && !is_sep(str[i], set))
+			count++;
+		while (str[i] && !is_sep(str[i], set))
+			i++;
+	}
+	return (count);
+}
+
+static char	*fill_letters(char const *str, char set)
+{
+	char	*newstr;
+	int		i;
+
+	i = 0;
+	while (!is_sep(str[i], set) && str[i])
+		i++;
+	newstr = (char *)malloc((i + 1) * sizeof(char));
+	if (!newstr)
+		return (0);
+	i = 0;
+	while (!is_sep(str[i], set) && str[i])
+	{
+		newstr[i] = str[i];
+		i++;
+	}
+	newstr[i] = '\0';
+	return (newstr);
+}
+
+char	**ft_split(char const *s, char set)
+{
+	char	**split_str;
+	int		count;
+	int		i;
+
+	if (!s)
+		return (NULL);
+	i = 0;
+	count = word_count(s, set);
+	split_str = (char **)malloc((count + 1) * sizeof(char *));
+	if (!split_str)
+		return (0);
+	while (*s)
+	{
+		while (*s && is_sep(*s, set))
+			s++;
+		if (*s)
+		{
+			split_str[i] = fill_letters(s, set);
+			i++;
+			while (*s && !is_sep(*s, set))
+				s++;
+		}
+	}
+	split_str[i] = 0;
+	return (split_str);
+}
+
+// char	*ft_strjoin(char const *s1, char const *s2)
+// {
+// 	size_t	total_len;
+// 	char	*str;
+// 	int		i;
+// 	int		j;
+
+// 	if (s1 && !s2)
+// 		return ((char *)s1);
+// 	if (!s1 && s2)
+// 		return ((char *)s2);
+// 	if (!s1 && !s2)
+// 		return (NULL);
+// 	total_len = ft_strlen(s1) + ft_strlen(s2) + 1;
+// 	str = (char *)malloc(total_len * sizeof(char));
+// 	if (!str)
+// 		return (NULL);
+// 	i = -1;
+// 	j = -1;
+// 	while (s1[++i])
+// 		str[i] = s1[i];
+// 	while (s2[++j])
+// 		str[i++] = s2[j];
+// 	str[i] = '\0';
+// 	return (str);
+// }
+
+
 // int main()
 // {
 
@@ -21,34 +124,34 @@
 // 	return (0);
 // }
 
-int main(int ac, char **av, char **envp)
-{
-	int id = fork();
-	int	n;
-	(void) ac;
-	(void) av;
+// int main(int ac, char **av, char **envp)
+// {
+// 	int id = fork();
+// 	int	n;
+// 	(void) ac;
+// 	(void) av;
 
-	while (*envp)
-	{
-		printf("%s\n", *envp);
-		envp++;
-	}
-	if (id == 0)
-		n = 1;
-	else
-		n = 6;
-	if (id != 0)
-		wait(NULL);
-	int i;
-	for (i = n; i < n + 5; i++)
-	{
-		printf("%d ", i);
-		fflush(stdout);
-	}
-	if (id != 0)
-		printf("\n");
-	return (0);
-}
+// 	while (*envp)
+// 	{
+// 		printf("%s\n", *envp);
+// 		envp++;
+// 	}
+// 	if (id == 0)
+// 		n = 1;
+// 	else
+// 		n = 6;
+// 	if (id != 0)
+// 		wait(NULL);
+// 	int i;
+// 	for (i = n; i < n + 5; i++)
+// 	{
+// 		printf("%d ", i);
+// 		fflush(stdout);
+// 	}
+// 	if (id != 0)
+// 		printf("\n");
+// 	return (0);
+// }
 
 // int main(void)
 // {
@@ -128,3 +231,38 @@ int main(int ac, char **av, char **envp)
 // 	}
 // }
 
+int main(int ac, char **av, char **envp)
+{
+	char	*input;
+	char	*cmd1;
+	char	*cmd2;
+	char	*output;
+	int		i;
+	char	**cmdpath;
+	// int		fd[2];
+
+	if (ac == 5)
+	{
+		input = av[1];
+		cmd1 = av[2];
+		cmd2 = av[3];
+		output = av[4];
+	}
+	i = -1;
+	while (envp[++i])
+	{
+		if (strncmp(envp[i], "PATH=", 5) == 0)
+		{
+			cmdpath = ft_split(envp[i] + 5, ':');
+			break ;
+		}
+	}
+	printf("%d\n", access("/usr/local/bin/sleep", X_OK));
+	printf("%d\n", access("/usr/bin/sleep", X_OK));
+	printf("%d\n", access("/bin/sleep", X_OK));
+	printf("%d\n", access("/usr/sbin/sleep", X_OK));
+	printf("%d\n", access("sbin/sleep", X_OK));
+	printf("%d\n", access("/usr/local/munki/sleep", X_OK));
+
+	return (0);
+}
